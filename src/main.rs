d9106996e49a91068b66a3af3e88f9a20038eb3a
Chung Wong <wch705@gmail.com>
2024-09-12 10:02:24 +1000

    demo

--- src/main.rs
+++ src/main.rs
@@ -2,64 +2,56 @@
 
 use dioxus::prelude::*;
 use std::error::Error;
-use tracing::Level;
-use ui::tabs::{TabItem, Tabs};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    dioxus_logger::init(tracing::Level::INFO)?;
+    launch(App);
+    Ok(())
+}
 
 fn App() -> Element {
     rsx! {
-        head::Link { rel: "stylesheet", href: asset!("./assets/tailwind.css") }
+        form {
+            Item {
+                Input {
+                    name: "first name",
+                }
+            }
 
-        Tabs {
-            default_active_id: "Tab 1",
-            items: vec![
-                TabItem {
-                    id: "Tab 1".to_string(),
-                    children: rsx! {
-                        "tab 1 content"
-                    },
-                },
-                TabItem {
-                    id: "Tab 2".to_string(),
-                    children: rsx! {
-                        "tab 2 content"
-                    },
-                },
-            ]
+            Item {
+                Input {
+                    name: "last name",
+                }
+            }
         }
     }
 }
 
-fn main() -> Result<(), Box<dyn Error>> {
-    dioxus_logger::init(Level::INFO)?;
-    #[cfg(feature = "web")]
-    {
-        launch(App);
+#[component]
+fn Styled(children: Element) -> Element {
+    rsx! {
+        head::Link { rel: "stylesheet", href: asset!("./assets/tailwind.css") }
+        {children}
     }
+}
 
-    #[cfg(feature = "server")]
-    {
-        use axum::Router;
-        use std::error::Error;
-
-        use clap::Parser;
-        use dioxus_cli_config::{AddressArguments, RuntimeCLIArguments};
-
-        tokio::runtime::Runtime::new()?.block_on(async move {
-            let app = Router::new().serve_dioxus_application(ServeConfig::new()?, App);
-
-            let cli_args = RuntimeCLIArguments::from_cli();
-            let address = cli_args
-                .as_ref()
-                .map(|args| args.fullstack_address())
-                .unwrap_or_else(AddressArguments::parse)
-                .address();
-
-            let listener = tokio::net::TcpListener::bind(&address).await?;
-
-            axum::serve(listener, app.into_make_service()).await?;
-
-            Ok::<(), Box<dyn Error>>(())
-        })?;
+#[component]
+fn Item(children: Element) -> Element {
+    rsx! {
+        Styled {
+            { children }
+        }
     }
-    Ok(())
+}
+
+#[component]
+fn Input(name: String, value: Option<String>) -> Element {
+    rsx! {
+        Styled {
+            input {
+                name,
+                value,
+            }
+        }
+    }
 }
